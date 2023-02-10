00525404592811df7a71301538fd17fafbd4dc6f
baranovdv <baranov.d.v.1988@gmail.com>
2023-02-10 13:55:45 +0700

    -feat: towel sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,35 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  let resultArray = []
+
+  // if(matrix = []) {
+  //   return []
+  // }
+if(matrix != undefined) {
+  for(let i = 0; i < matrix.length; i++){
+
+    if(i % 2){
+      for(let j = matrix[i].length - 1; j >= 0 ; j--)
+
+      {
+        // console.log(matrix[i][j])
+        resultArray.push(matrix[i][j])
+      }
+    } else {
+      for(let j = 0;j < matrix[i].length; j++)
+
+      {
+        // console.log(matrix[i][j])
+        resultArray.push(matrix[i][j])
+      }
+    }
+  }
 }
+
+
+  return resultArray;
+}
+
+// console.log(module.exports([[1, 2, 3],[4, 5, 6], [7, 8, 9]]))
